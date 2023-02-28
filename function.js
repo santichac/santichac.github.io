5fb02f36eea5e7077063a9ffdd0a1aa0d9bd5de4
Santi Chac <103904254+santichac@users.noreply.github.com>
2023-02-28 15:38:36 -0300

    Initial Commit

---
+++ function.js
@@ -0,0 +1,9 @@
+document.getElementById("oscuro").onclick = function(){
+    document.body.style.backgroundColor ="#000000";
+    console.log("Cambio a negro");  
+}
+
+document.getElementById("claro").onclick = function(){
+    document.body.style.backgroundColor = "#808080";
+    console.log("Volvio a gris");  
+}
